a44de8e8b6b862200b4a81c20cf8b73dbfb30b87
James Joseph Balamuta <balamut2@illinois.edu>
2017-06-23 12:21:43 -0500

    Rename to edina from base_components

--- src/edina.cpp
+++ src/edina.cpp
@@ -1,41 +1,6 @@
 #include "ecdm.h"
 #include <rgen.h>
 
-//' Bijection Vector
-//'
-//' @param K number of levels
-//' @return A \code{vec} with length \eqn{K}.
-//' @export
-// [[Rcpp::export]]
-arma::vec bijectionvector(unsigned int K) {
-  arma::vec vv(K);
-
-  for (unsigned int k = 0; k < K; ++k) {
-    vv(k) = pow(2, K - k - 1);
-  }
-
-  return vv;
-}
-
-//' Inverse Bijection Vector
-//'
-//' @param CL A \code{double} that controls ...
-//' @inheritParams bijectionvector
-//' @return A \code{vec} with length \eqn{K}.
-//' @export
-// [[Rcpp::export]]
-arma::vec inv_bijectionvector(unsigned int K, double CL) {
-  arma::vec alpha(K);
-
-  for (unsigned int k = 0; k < K; ++k) {
-    double twopow = pow(2, K - k - 1);
-    alpha(k) = (twopow <= CL);
-    CL = CL - twopow * alpha(k);
-  }
-
-  return alpha;
-}
-
 //' \eqn{\eta} Matrix
 //'
 //' @param K      Number of Attribute Levels
@@ -62,7 +27,6 @@ arma::mat ETAmat(unsigned int K, unsigned int J, const arma::mat &Q) {
 
   return ETA;
 }
-
 
 //' \eqn{\eta} Matrix
 //'
