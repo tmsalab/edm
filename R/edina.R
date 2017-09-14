2fc22ba2557c24d4afade4f4eb89f14a952200aa
James J Balamuta <balamut2@illinois.edu>
2017-09-14 14:34:25 -0500

    Change edina object ordering

--- R/edina.R
+++ R/edina.R
@@ -1,20 +1,34 @@
 #' Construct an EDINA object
 #'
 #' Constructor function to satisfy the EDINA object definition
+#' @param coefs        Matrix with means of guessing and slipping coefficients.
+#' @param pis          Estimated latent classes
+#' @param est_q        Estimated Q Matrix
+#' @param or_tested    Sample empirical odds ratio compared against simulated
+#'                     odds ratio divided by the number of simulations.
+#' @param sample_or    Sample empirical odds ratio based on the trial matrix.
 #' @inheritParams edina
-#' @param x            `edina` object.
 #' @param timing       Number of Seconds that have elapsed since run time.
-#' @param sample_OR    Odds Ratio based on the trial matrix.
-#' @param model_fit    How well the model fit according to the heuristic
 #' @keywords internal
-new_edina = function(x, k, burnin, chain_length, timing, sample_OR, model_fit) {
-    structure(list("edina"        = x,
+new_edina = function(coefs, pis, est_q, or_tested, sample_or, k, burnin, chain_length,  timing) {
+
+    item_nums = paste0("Item", seq_len(nrow(coefs)) )
+
+    colnames(coefs) = c("Guessing", "Slipping")
+    rownames(coefs) = item_nums
+
+    colnames(est_q) = paste0("Trait", seq_len(ncol(est_q)))
+    rownames(est_q) = item_nums
+
+    structure(list("coefficients" = coefs,
+                   "pi_classes"   = pis,
+                   "est_q"        = est_q,
+                   "or_tested"    = or_tested,
+                   "sample_or"    = sample_or,
                    "k"            = k,
                    "burnin"       = burnin,
                    "chain_length" = chain_length,
-                   "timing"       = timing,
-                   "sample_OR"    = sample_OR,
-                   "model_fit"    = model_fit),
+                   "timing"       = timing),
                    class = "edina")
 }
 
@@ -22,49 +36,32 @@ new_edina = function(x, k, burnin, chain_length, timing, sample_OR, model_fit) {
 #' Construct a Summary EDINA object
 #'
 #' Constructor function to satisfy the Summary EDINA object definition
-#' @param m_gs    Mean of the Guessing Values
-#' @param m_ss    Mean of the Slipping Values
-#' @param m_pi    Mean of the PIs
-#' @inheritParams edina
-#' @inheritParams new_edina
+#' @param edina      An `edina` object
+#' @param model_fit  Computed model heuristic value
+#' @param alpha      The region used in the computation of the heuristic.
 #' @keywords internal
-new_edina_summary = function(m_gs, m_ss, m_pi,
-                             k, burnin, chain_length, timing, sample_OR, model_fit) {
-    structure(list("m_gs"         = m_gs,
-                   "m_ss"         = m_ss,
-                   "m_pi"         = m_pi,
-                   "k"            = k,
-                   "burnin"       = burnin,
-                   "chain_length" = chain_length,
-                   "timing"       = timing,
-                   "sample_OR"    = sample_OR,
-                   "model_fit"    = model_fit),
-              class = "summary_edina")
+new_edina_summary = function(edina, model_fit, alpha) {
+
+    edina[["model_fit"]] = model_fit
+    edina[["alpha"]] = alpha
+
+    class(edina) = c("summary_edina", "edina")
+
+    edina
 }
 
 
 #' Model Heuristic used for Model Selection
 #'
 #' Computes the model heuristic for model selection.
-#' @param sample_OR   A `matrix` with dimensions \code{J x J}
-#'                    containing the empirical odds ratio.
-#' @param bayesian_OR An `array` with dimensions \code{J x J}.
-#'                    containing the bayesian odds ratio.
+#' @inheritParams summary.edina
 #' @return A `double` that is the mean.
 #' @export
-model_heuristic = function(sample_OR, bayesian_OR) {
+model_heuristic = function(object, alpha = 0.05) {
+    or_tested = object$or_tested
 
-    obj = matrix(0, ncol(sample_OR), ncol(sample_OR))
-
-    chain_length = dim(bayesian_OR)[3]
-
-    for ( i in seq_len(chain_length) ) {
-        obj = obj + as.matrix( (bayesian_OR[,,i]) > sample_OR)*1
-    }
-
-    d = obj / dim(bayesian_OR)[3]
-
-    mean(d[upper.tri(d)] < 0.05 | d[upper.tri(d)] > 0.95)
+    mean(or_tested[upper.tri(or_tested)] < alpha |
+         or_tested[upper.tri(or_tested)] > (1-alpha))
 }
 
 #' EDINA estimation routine
@@ -102,48 +99,55 @@ edina = function(data, k = 3, burnin = 10000, chain_length = 20000){
     stopifnot(is.whole(chain_length) && length(chain_length) == 1)
 
     time_info = system.time({
+
         edina_model = edina_Gibbs_Q(data, k,
                                     burnin = burnin,
                                     chain_length = chain_length)
+
     })[1:3]
 
-
-    sample_OR = OddsRatio(nrow(data), ncol(data), data)
-
-    new_edina(edina_model, k,
+    new_edina(edina_model$coefficients,
+              edina_model$pis,
+              edina_model$est_q,
+              edina_model$or_tested,
+              edina_model$sample_or,
+              k,
               burnin, chain_length,
-              time_info,
-              sample_OR,
-              model_heuristic(sample_OR, edina_model$ORs)
+              time_info
               )
 }
 
+#' Printing out the EDINA Object
+#'
+#' Custom print method for computing the EDINA.
+#' @param x An `edina` object
+#' @export
 print.edina = function(x, ...){
-    cat("The edina function for K = ", x$k, " took ", convert_seconds_to_time(x$timing[3]))
+    cat("The EDINA model for K =", x$k,
+        "took", format(convert_seconds_to_time(x$timing[3])), "\n")
 
+    cat("\nThe estimated coefficients for the EDINA model are:\n")
+    print(x$coefficients)
+
+    cat("\nThe estimated Q matrix is:\n")
+    print(x$est_q)
 }
 
 #' Summarizing the EDINA Object
 #'
+#' Determine whether the `edina` object is summarized appropriately.
 #' @param object An `edina` object
+#' @param alpha  Defining region to indicate the level of extremeness
+#'               the data must before the model is problematic.
 #' @export
-summary.edina = function(object, ...) {
+summary.edina = function(object, alpha = 0.05, ...) {
 
-    m_gs = apply(object$edina$GS, 1, mean)
-    # m_gs
-    m_ss = apply(object$edina$SS, 1, mean)
-    # m_ss
-    m_pi = apply(object$edina$PIs, 1, mean)
+    new_edina_summary(
+        object,
+        model_fit = model_heuristic(object, alpha),
+        alpha = alpha
+    )
 
-    new_edina_summary(m_gs,
-                      m_ss,
-                      m_pi,
-                      object$k,
-                      object$burnin,
-                      object$chain_length,
-                      object$timing,
-                      object$sample_OR,
-                      object$model_fit)
 }
 
 #' Printing out the Summary EDINA Object
@@ -151,19 +155,9 @@ summary.edina = function(object, ...) {
 #' @param x A `summary_edina` object
 #' @export
 print.summary_edina = function(x, ...) {
-    cat("The means of the Guessing Parameters are: ", fill = TRUE)
-    print(x$m_gs)
-    cat("\n")
-
-    cat("The means of the Slipping Parameters are: ",fill = TRUE)
-    print(x$m_ss)
-    cat("\n")
-
-    cat("The means of the PIs are: ", fill = TRUE)
-    print(x$m_pi)
-    cat("\n")
-
-    cat("The model fit under the heuristic is: ", fill = TRUE)
-    print(x$model_fit)
-    cat("\n")
+    # Rely upon the specification of the `edina` object in the summary class.
+    # NextMethod()
+    cat("The EDINA model for K =", x$k,
+        "took", format(convert_seconds_to_time(x$timing[3])), "\n")
+    cat("\nThe model fit under the heuristic alpha =", x$alpha ,"is:", x$model_fit, "\n")
 }
