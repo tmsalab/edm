601ed187585295cd0ae1bde49cc7ce94ced73109
James J Balamuta <james.balamuta@gmail.com>
2018-04-10 23:21:55 -0500

    Fix package importation to avoid using `dina` on CRAN

--- R/ecdm-package.R
+++ R/ecdm-package.R
@@ -1,5 +1,6 @@
 #' @useDynLib ecdm, .registration=TRUE
 #' @importFrom Rcpp sourceCpp
+#' @importFrom stats reshape
 #' @details
 #' The `ecdm` package re-exports several functions from other estimation
 #' packages developed by `ecdmcore`. As a result, the `ecdm` package is
