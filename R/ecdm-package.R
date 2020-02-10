2e82a2c8fc61cab7d5f3873e707a3dd43bb611a6
James Balamuta <balamut2@illinois.edu>
2020-02-10 14:39:53 -0600

    Remove Rcpp

--- R/ecdm-package.R
+++ R/ecdm-package.R
@@ -8,7 +8,5 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @useDynLib ecdm, .registration = TRUE
-#' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
 NULL
