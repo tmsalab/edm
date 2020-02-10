652eb71f099608a928071ef4c772b391a02b6df8
James Balamuta <balamut2@illinois.edu>
2020-02-10 14:16:51 -0600

    Improve package-level doc

--- R/ecdm-package.R
+++ R/ecdm-package.R
@@ -1,8 +1,14 @@
-#' @useDynLib ecdm, .registration=TRUE
-#' @importFrom Rcpp sourceCpp
-#' @importFrom stats reshape
 #' @details
 #' The `ecdm` package re-exports several functions from other estimation
 #' packages developed by `ecdmcore`. As a result, the `ecdm` package is
 #' partially a virtual package.
+#' @keywords internal
 "_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @useDynLib ecdm, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
